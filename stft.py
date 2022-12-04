2ae293919b8544989e93d3cc4a58e2f5a07e9388
johnpaulbin <johnpaulbin@gmail.com>
2022-12-03 21:44:34 -0800

    Update stft.py

--- stft.py
+++ stft.py
@@ -179,12 +179,13 @@ class STFT(torch.nn.Module):
 
 
 class TorchSTFT(torch.nn.Module):
-    def __init__(self, filter_length=800, hop_length=200, win_length=800, window='hann'):
+    def __init__(self, filter_length=800, hop_length=200, win_length=800, window='hann', device="cpu"):
         super().__init__()
         self.filter_length = filter_length
         self.hop_length = hop_length
         self.win_length = win_length
         self.window = torch.from_numpy(get_window(window, win_length, fftbins=True).astype(np.float32))
+        self.device = device
 
     def transform(self, input_data):
         forward_transform = torch.stft(
@@ -197,7 +198,7 @@ class TorchSTFT(torch.nn.Module):
     def inverse(self, magnitude, phase):
         inverse_transform = torch.istft(
             magnitude * torch.exp(phase * 1j),
-            self.filter_length, self.hop_length, self.win_length, window=self.window)
+            self.filter_length, self.hop_length, self.win_length, window=self.window.to(self.device))
 
         return inverse_transform.unsqueeze(-2)  # unsqueeze to stay consistent with conv_transpose1d implementation
 
